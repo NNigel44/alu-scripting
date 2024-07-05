d97548314a399f0d54700d0abe8c0b6826ae1592
NNigel44 <nsenganigel@gmail.com>
2024-07-05 07:20:21 +0000

    add 0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,18 +1,32 @@
 #!/usr/bin/python3
+"""
+Queries the Reddit API and returns the number of subscribers for a given subreddit.
+
+Args:
+    subreddit (str): The name of the subreddit.
+
+Returns:
+    int: The number of subscribers if the subreddit is valid, 0 otherwise.
+"""
+
 import requests
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers for a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'my_user_agent'}
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.RequestException:
+    """
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    
+    Args:
+    subreddit (str): The name of the subreddit.
+    
+    Returns:
+    int: The number of subscribers if the subreddit is valid, 0 otherwise.
+    """
+    headers = {'User-Agent': 'My Reddit API Client'}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
         return 0
 
